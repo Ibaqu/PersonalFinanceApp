1fffd2562d978493c1c5e6a6bddbde4f872de96c
Ibaqu <aquib.zt@gmail.com>
2024-11-08 19:45:08 -0500

    Create Dashboard & Spending components placeholder
    - Also implements the switch between components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import MainContent from "./components/MainContent";
+import DashboardComponent from "./components/DashboardComponent";
+import SpendingComponent from "./components/SpendingComponent";
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("");
+    const [page, setPage] = useState("Dashboard"); // New state to track current page
 
-  useEffect(() => {
-    fetch("/api/message")
-        .then((response) => response.json())
-        .then((data) => setMessage(data.message))
-        .catch((error) => console.error("Error fetching message:", error));
-  }, []);
+    useEffect(() => {
+        fetch("/api/message")
+            .then((response) => response.json())
+            .then((data) => setMessage(data.message))
+            .catch((error) => console.error("Error fetching message:", error));
+    }, []);
 
-  return (
-      <div className="App">
-          <Header/>
-          <div className="dashboard-layout">
-              <Sidebar/>
-              <MainContent/>
-          </div>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className="dashboard-layout">
+                <Sidebar setPage={setPage} /> {/* Pass setPage to Sidebar */}
+                <main className="main-content">
+                    {page === "Dashboard" && <DashboardComponent />}
+                    {page === "Spending" && <SpendingComponent />}
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/SpendingComponent.js
@@ -0,0 +1,9 @@
+function SpendingComponent() {
+    return (
+        <div>
+            <h1>SPENDING COMPONENT</h1>
+        </div>
+    );
+}
+
+export default SpendingComponent;
