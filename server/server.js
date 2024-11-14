d1e832e2d3e8361bd2ce7e362a196d29e7bcd219
Ibaqu <aquib.zt@gmail.com>
2024-11-13 23:36:36 -0500

    Added accounts balance api to the backend

--- server/server.js
+++ server/server.js
@@ -44,6 +44,36 @@ app.get("/api/accounts", (req, res) => {
 
 });
 
+app.get("/api/balance/get", (req, res) => {
+
+    // Read Sample JSON file
+    // TODO : Call plaid api directly when implemented
+
+    const dataPath = path.join(__dirname, "resources/sampleData/accounts_balance.json");
+    fileStream.readFile(dataPath, (err, data) => {
+
+        // If there is an error reading the file
+        if (err) {
+            console.error("Error reading accounts_get file : ", err);
+            res.status(500).json(
+                {
+                    error: "Failed to load accounts data"
+                }
+            );
+            return;
+        }
+
+        // Parse JSON data and extract relevant fields
+        const accountsBalanceData = JSON.parse(data);
+        const accountsBalance = accountsBalanceData.accounts.map(account => ({
+            name: account.name,
+            balance: account.balances.current
+        }));
+
+        res.json({accountsBalance});
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
